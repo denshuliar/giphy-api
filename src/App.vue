c9d79836fcf06cdaefa75bb512569866b680c665
unknown <denshullyar@gmail.com>
2020-07-08 10:15:20 +0300

    fixed some bags

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,6 @@
 <script>
 import Search from "./components/Search.vue";
 import Preview from "./components/Preview.vue";
-const Vue = require("vue/dist/vue.common.js");
 
 export default {
   name: "app",
